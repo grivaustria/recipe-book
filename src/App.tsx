a7d3948629d928fab1fd3c19f8fb8b05c2ffc203
Gabriel Austria <brhymed01@gmail.com>
2025-07-15 11:13:51 +0800

    Added: temp DishData

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
+
 import './App.scss'
+
 import Navigation from './component/navigation/navigation.component'
 import SearchBar from './component/search-bar/search-bar.component'
 import Card from './component/card/card.component'
 
-function App() {
+import { dishJSON } from './data/dish-temp'
 
+function App() {
+  const dishData = dishJSON;
   return (
     <>
       <Navigation />
---
+++ src/data/dish-temp.tsx
@@ -0,0 +1,269 @@
+import ChickenAdobo from "../assets/dish-chicken-adobo.jpg";
+
+export const dishJSON = [
+  {
+    id: 1,
+    dishName: "ChickenAdobo",
+    dishImage: ChickenAdobo,
+    ingredients: [
+      {
+        name: "Chicken",
+        quantity: "2",
+        unit: "lbs",
+      },
+      {
+        name: "Soy Sauce",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "White Vinegar",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "Garlic Powder",
+        quantity: "1",
+        unit: "tsp",
+      },
+      {
+        name: "Water",
+        quantity: "360",
+        unit: "mL",
+      },
+      {
+        name: "Laurel Leaves",
+        quantity: "3-4",
+        unit: "pcs",
+      },
+      {
+        name: "Potato",
+        quantity: "1-2",
+        unit: "pcs",
+      },
+    ],
+    procedure: [
+      {
+        proc1: "Combine soy sauce, white vinegar, and garlic powder. Mix well.",
+        proc2:
+          "Marinate the chicken with the mixture for at least an hour. The longer, the better.",
+        proc3:
+          "Pour the chicken and the marinade to a hotpot. Add water and bring it to a boil.",
+        proc4: "Add laurel leaves and simmer for 30 minutes.",
+        proc5: "Stir and turn the heat off. Serve hot.",
+      },
+    ],
+  },
+  {
+    id: 1,
+    dishName: "ChickenAdobo",
+    dishImage: ChickenAdobo,
+    ingredients: [
+      {
+        name: "Chicken",
+        quantity: "2",
+        unit: "lbs",
+      },
+      {
+        name: "Soy Sauce",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "White Vinegar",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "Garlic Powder",
+        quantity: "1",
+        unit: "tsp",
+      },
+      {
+        name: "Water",
+        quantity: "360",
+        unit: "mL",
+      },
+      {
+        name: "Laurel Leaves",
+        quantity: "3-4",
+        unit: "pcs",
+      },
+      {
+        name: "Potato",
+        quantity: "1-2",
+        unit: "pcs",
+      },
+    ],
+    procedure: [
+      {
+        proc1: "Combine soy sauce, white vinegar, and garlic powder. Mix well.",
+        proc2:
+          "Marinate the chicken with the mixture for at least an hour. The longer, the better.",
+        proc3:
+          "Pour the chicken and the marinade to a hotpot. Add water and bring it to a boil.",
+        proc4: "Add laurel leaves and simmer for 30 minutes.",
+        proc5: "Stir and turn the heat off. Serve hot.",
+      },
+    ],
+  },
+  {
+    id: 1,
+    dishName: "ChickenAdobo",
+    dishImage: ChickenAdobo,
+    ingredients: [
+      {
+        name: "Chicken",
+        quantity: "2",
+        unit: "lbs",
+      },
+      {
+        name: "Soy Sauce",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "White Vinegar",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "Garlic Powder",
+        quantity: "1",
+        unit: "tsp",
+      },
+      {
+        name: "Water",
+        quantity: "360",
+        unit: "mL",
+      },
+      {
+        name: "Laurel Leaves",
+        quantity: "3-4",
+        unit: "pcs",
+      },
+      {
+        name: "Potato",
+        quantity: "1-2",
+        unit: "pcs",
+      },
+    ],
+    procedure: [
+      {
+        proc1: "Combine soy sauce, white vinegar, and garlic powder. Mix well.",
+        proc2:
+          "Marinate the chicken with the mixture for at least an hour. The longer, the better.",
+        proc3:
+          "Pour the chicken and the marinade to a hotpot. Add water and bring it to a boil.",
+        proc4: "Add laurel leaves and simmer for 30 minutes.",
+        proc5: "Stir and turn the heat off. Serve hot.",
+      },
+    ],
+  },
+  {
+    id: 1,
+    dishName: "ChickenAdobo",
+    dishImage: ChickenAdobo,
+    ingredients: [
+      {
+        name: "Chicken",
+        quantity: "2",
+        unit: "lbs",
+      },
+      {
+        name: "Soy Sauce",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "White Vinegar",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "Garlic Powder",
+        quantity: "1",
+        unit: "tsp",
+      },
+      {
+        name: "Water",
+        quantity: "360",
+        unit: "mL",
+      },
+      {
+        name: "Laurel Leaves",
+        quantity: "3-4",
+        unit: "pcs",
+      },
+      {
+        name: "Potato",
+        quantity: "1-2",
+        unit: "pcs",
+      },
+    ],
+    procedure: [
+      {
+        proc1: "Combine soy sauce, white vinegar, and garlic powder. Mix well.",
+        proc2:
+          "Marinate the chicken with the mixture for at least an hour. The longer, the better.",
+        proc3:
+          "Pour the chicken and the marinade to a hotpot. Add water and bring it to a boil.",
+        proc4: "Add laurel leaves and simmer for 30 minutes.",
+        proc5: "Stir and turn the heat off. Serve hot.",
+      },
+    ],
+  },
+  {
+    id: 1,
+    dishName: "ChickenAdobo",
+    dishImage: ChickenAdobo,
+    ingredients: [
+      {
+        name: "Chicken",
+        quantity: "2",
+        unit: "lbs",
+      },
+      {
+        name: "Soy Sauce",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "White Vinegar",
+        quantity: "3/4",
+        unit: "cup",
+      },
+      {
+        name: "Garlic Powder",
+        quantity: "1",
+        unit: "tsp",
+      },
+      {
+        name: "Water",
+        quantity: "360",
+        unit: "mL",
+      },
+      {
+        name: "Laurel Leaves",
+        quantity: "3-4",
+        unit: "pcs",
+      },
+      {
+        name: "Potato",
+        quantity: "1-2",
+        unit: "pcs",
+      },
+    ],
+    procedure: [
+      {
+        proc1: "Combine soy sauce, white vinegar, and garlic powder. Mix well.",
+        proc2:
+          "Marinate the chicken with the mixture for at least an hour. The longer, the better.",
+        proc3:
+          "Pour the chicken and the marinade to a hotpot. Add water and bring it to a boil.",
+        proc4: "Add laurel leaves and simmer for 30 minutes.",
+        proc5: "Stir and turn the heat off. Serve hot.",
+      },
+    ],
+  },
+];
