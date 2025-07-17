aa9d306980e284a587b44988c077f5a0d80ca0a1
Gabriel Austria <brhymed01@gmail.com>
2025-07-17 23:03:23 +0800

    Refactor: SearchBar Width

--- src/component/search-bar/search-bar.component.tsx
+++ src/component/search-bar/search-bar.component.tsx
@@ -10,5 +10,5 @@ const SearchBar = ({onChangeHandler}: SearchBoxProps) => {
         <SearchBox onChange={onChangeHandler} />
     )
 }
-
+ 
 export default SearchBar;
--- src/component/search-bar/search-bar.styles.jsx
+++ src/component/search-bar/search-bar.styles.jsx
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const SearchBox = styled.input.attrs({
   type: "search",
-  placeholder: "Search recipes...",
+  placeholder: "Search by dish name...",
 })`
   background-color: #f8f8f8;
   border: 3px solid #878787;
-  border-radius: 10px;
-  padding: 1rem;
-  width: 100%;
+  border-radius: 20px;
+  padding: 0.75rem 1rem;
+  width: 600px;
   font-size: 18px;
-  
+  transition: 0.2s width ease-in-out;
+
 `;
