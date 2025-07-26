c4a846419a51817d209fbdd20413c0e534e41107
Gabriel Austria <brhymed01@gmail.com>
2025-07-26 16:39:55 +0800

    Added: ProcedureList component

---
+++ src/component/modal/add-recipe/procedure-list.component.tsx
@@ -0,0 +1,63 @@
+import type { Procedure } from "../../../types/dish.type";
+import {
+  ContentContainer,
+  InputText,
+  RemoveButton,
+  AddItemButton,
+  LabelText,
+  OptionsContainer,
+} from "../add-recipe/add-recipe.styles";
+
+type ProcedureListProps = {
+  procedure: Procedure[];
+  onChange: (index: number, value: string) => void;
+  onAdd: () => void;
+  onRemove: (index: number) => void;
+};
+
+const ProcedureList = ({
+  procedure,
+  onChange,
+  onAdd,
+  onRemove,
+}: ProcedureListProps) => {
+  return (
+    <>
+      <ContentContainer>
+        <LabelText>Procedure</LabelText>
+        <LabelText></LabelText>
+        <LabelText></LabelText>
+      </ContentContainer>
+
+      {procedure.map((stepItem, index) => (
+        <ContentContainer key={index}>
+          <InputText type="text" value={index + 1} readOnly />
+          <InputText
+            type="text"
+            placeholder="e.g. Marinate the chicken for 30 minutes"
+            value={stepItem.step}
+            onChange={(event) => onChange(index, event.target.value)
+            }
+            required={index === 0}
+          />
+          <OptionsContainer>
+            <RemoveButton
+              type="button"
+              onClick={() => onRemove(index)}
+            >
+              &#10005;
+            </RemoveButton>
+
+            {index === procedure.length - 1 && (
+              <AddItemButton type="button" onClick={onAdd}>
+                Add Item
+              </AddItemButton>
+            )}
+          </OptionsContainer>
+        </ContentContainer>
+      ))}
+    </>
+  );
+};
+
+export default ProcedureList;
