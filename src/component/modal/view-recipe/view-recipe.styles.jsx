262c11633067482eba09ccf3bd8b7da96a441d12
Gabriel Austria <brhymed01@gmail.com>
2025-08-12 20:47:47 +0800

    Refactor: ViewRecipe IngredientList & ProcedureList

--- src/component/modal/view-recipe/view-recipe.styles.jsx
+++ src/component/modal/view-recipe/view-recipe.styles.jsx
@@ -37,7 +37,7 @@ export const ImageDisplay = styled.img`
 
   ${device.tablet} {
     max-height: none;
-    width: 450px; 
+    width: 450px;
     height: 600px;
   }
 `;
@@ -46,7 +46,7 @@ export const ContentSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
-  flex-grow: 1;
+  // flex-grow: 1;
   min-width: 0;
   width: 100%;
   padding: 1rem 0.5rem;
@@ -65,12 +65,26 @@ export const ContentTitle = styled.div`
   flex-direction: column;
   align-items: flex-start;
   gap: 0.5rem;
-  padding: 0 0.5rem;
+
+  ${device.mobile} {
+    padding: 0 0.5rem;
+  }
+
+  ${device.tablet} {
+    padding: 0.5rem;
+  }
 `;
 
 export const ContentText = styled.span`
-  font-size: 32px;
   font-weight: 700;
+
+  ${device.mobile} {
+    font-size: 24px;
+  }
+
+  ${device.tablet} {
+    font-size: 32px;
+  }
 `;
 
 export const ContentTag = styled.span`
@@ -104,23 +118,52 @@ export const ContentOption = styled.div`
 export const ContentList = styled.div`
   display: flex;
   flex-direction: column;
+
+  ${device.mobile} {
+    margin: 0 1rem 0 0;
+  }
+
+  ${device.tablet} {
+    margin: 0;
+  }
 `;
 
 export const ContentHeading = styled.span`
-  font-size: 28px;
   font-weight: 700;
   padding: 0.5rem;
   border-bottom: 3px solid #8c6662;
 
   cursor: default;
+
+  ${device.mobile} {
+    font-size: 24px;
+  }
+
+  ${device.tablet} {
+    font-size: 28px;
+  }
 `;
 
 export const ContentBullet = styled.ul`
-  font-size: 18px;
   line-height: 28px;
+
+  ${device.mobile} {
+    font-size: 16px;
+  }
+
+  ${device.tablet} {
+    font-size: 18px;
+  }
 `;
 
 export const ContentOrder = styled.ol`
-  font-size: 18px;
   line-height: 28px;
+
+  ${device.mobile} {
+    font-size: 16px;
+  }
+
+  ${device.tablet} {
+    font-size: 18px;
+  }
 `;
