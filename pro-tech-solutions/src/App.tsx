075a3463d38ebe4d64be164888af32d0c5558c99
Gabriel Austria <grivaustria@gmail.com>
2025-09-05 09:01:27 +0800

    pro-tech-solutions

---
+++ pro-tech-solutions/src/App.tsx
@@ -0,0 +1,16 @@
+import Message from './components/message.component'
+import ButtonGrid from './components/button-grid.component'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=' h-[100vh] mx-32 py-8 '>
+      <Message />
+      <ButtonGrid />
+    </div>
+  )
+}
+
+export default App
