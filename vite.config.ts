e2cea2ad80f40eb2d73fa286d74c7335af1bcf98
Gabriel Austria <grivaustria@gmail.com>
2026-03-19 17:44:57 +0800

    Configured error displayed in Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,48 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line @typescript-eslint/typedef
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("firebase")) {
+              return "firebase";
+            }
+
+            if (
+              id.includes("@mui") ||
+              id.includes("@emotion") ||
+              id.includes("styled-components")
+            ) {
+              return "ui-vendor";
+            }
+
+            if (id.includes("react-toastify")) {
+              return "toast";
+            }
+
+            if (id.includes("framer-motion")) {
+              return "motion";
+            }
+
+            if (id.includes("@iconify")) {
+              return "iconify";
+            }
+
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router")
+            ) {
+              return "react-vendor";
+            }
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine-sc",
